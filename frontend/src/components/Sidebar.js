0647018f9f031f82f040662d4bbb2cc458e9d6fa
jdrae <draejang@gmail.com>
2025-07-10 17:36:20 +0900

    implement price and latency changing

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,27 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import bitcoin from '../assets/bitcoin.png';
-import upArrow from '../assets/up-arrow.svg';
+import { io } from 'socket.io-client';
 
-const PriceCard = () => (
+const PriceCard = ({ price }) => (
   <div className="price-card sidebar-card default-card">
     <div className="price-card-top">
       <div>
         <img src={bitcoin} alt="Bitcoin" style={{ width: 16, height: 16 }} />
         <span className="small-text">BTCUSDT</span>
       </div>
-      <div>
-        <img src={upArrow} alt="Up" style={{ width: 10, height: 10 }} />
-        <span className="small-text" style={{ color: '#09A249' }}>0.3%</span>
-      </div>
     </div>
-    <span className="big-text price-value">105,023</span>
+    <span className="big-text price-value">{price}</span>
   </div>
 );
 
-const LatencyCard = () => (
+const LatencyCard = ({ latency }) => (
   <div className="latency-card sidebar-card default-card">
     <span className="small-text" style={{ marginBottom: 8 }}>Latency</span>
-    <span className="big-text latency-value">300ms</span>
+    <span className="big-text latency-value">{latency}ms</span>
   </div>
 );
 
@@ -31,16 +27,41 @@ const SmallBox = ({ label, className }) => (
   </a>
 );
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <PriceCard />
-    <LatencyCard />
-    <div className="small-box-container">
-      <SmallBox label="BTC" className="btc-box" />
-      <SmallBox label="ETH" />
-      <SmallBox label="XRP" />
-    </div>
-  </aside>
-);
+const Sidebar = () => {
+  const [price, setPrice] = useState('');
+  const [latency, setLatency] = useState('');
 
-export default Sidebar; 
+  useEffect(() => {
+    const socket = io('http://localhost:5555');
+    socket.on('connect', () => {
+      console.log('Socket connected:', socket.id);
+      socket.emit("subscribe", { topic: "btc" });
+    });
+    socket.on("message", (data) => {
+      const price = Number(data['data']['k']['c']).toFixed(2);
+      const latency = Date.now() - data["received_at"];
+      setPrice(price);
+      setLatency(latency);
+    });
+    return () => {
+      socket.on("disconnect", () => {
+        console.log("Socket disconnected");
+      });
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <aside className="sidebar">
+      <PriceCard price={price} />
+      <LatencyCard latency={latency} />
+      <div className="small-box-container">
+        <SmallBox label="BTC" className="btc-box" />
+        <SmallBox label="ETH" />
+        <SmallBox label="XRP" />
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
